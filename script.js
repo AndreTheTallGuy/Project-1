3e5b18453b3ed8a3cbd4f81979ed4a24d7b8a17e
Andre Entrekin <andre.entrekin@gmail.com>
2020-01-02 11:04:33 -0600

    moved wiki api to script.js

--- script.js
+++ script.js
@@ -12,23 +12,35 @@ $(".searchBtn").on("click", function (event) {
     }).then(function (response) {
         console.log(response._embedded.events);
 
-        var hotelURL = "https://hotels4.p.rapidapi.com/locations/search?locale=en_US&query=" + city
-
-        var settings = {
-            "async": true,
-            "crossDomain": true,
-            "url": hotelURL,
-            "method": "GET",
-            "headers": {
-                "x-rapidapi-host": "hotels4.p.rapidapi.com",
-                "x-rapidapi-key": "161dfc0a21msh4ed0b35c9e22b38p157c85jsn53ebc8a10b20"
-            }
-        }
-
-        $.ajax(settings).done(function (response) {
-            console.log(response);
-        });
     });
+
+    // Hotels
+    var hotelURL = "https://hotels4.p.rapidapi.com/locations/search?locale=en_US&query=" + city
+
+    var settings = {
+        "async": true,
+        "crossDomain": true,
+        "url": hotelURL,
+        "method": "GET",
+        "headers": {
+            "x-rapidapi-host": "hotels4.p.rapidapi.com",
+            "x-rapidapi-key": "161dfc0a21msh4ed0b35c9e22b38p157c85jsn53ebc8a10b20"
+        }
+    }
+
+    $.ajax(settings).done(function (response) {
+        console.log(response);
+    });
+
+    //Wikipedia
+    var queryUrl = "https://www.mediawiki.org/w/api.php?action=login&lgname=Jsegel12@Project1&lgpassword=n8mqe7fg3ql50genu5iaskushljgicjf&lgtoken=123ABC";
+    $.ajax({
+        url: queryUrl,
+        method: "GET"
+    }).then(function (response) {
+        console.log(response);
+    })
+
 
 });
 
@@ -47,3 +59,4 @@ $(".searchBtn").on("click", function (event) {
 // $.ajax(settings).done(function (response) {
 //     console.log(response);
 // });
+
