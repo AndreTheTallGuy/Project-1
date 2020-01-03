98dd0410984e9c97cd2a8c330f0c133ff4813a63
Andre Entrekin <andre.entrekin@gmail.com>
2020-01-02 20:12:03 -0600

    added booking.com

--- script.js
+++ script.js
@@ -51,31 +51,20 @@ $(".searchBtn").on("click", function (event) {
     });
 
 
-
-
-
-
-
-
-
     // Hotels
-    var hotelURL = "https://hotels4.p.rapidapi.com/locations/search?locale=en_US&query=" + city
-
-
-
     var settings = {
         "async": true,
         "crossDomain": true,
-        "url": hotelURL,
+        "url": "https://qtravel-qtravel.p.rapidapi.com/apis?query=chicago&qapikey=125371768e33eb299b923ed3ca28e71c",
         "method": "GET",
         "headers": {
-            "x-rapidapi-host": "hotels4.p.rapidapi.com",
+            "x-rapidapi-host": "qtravel-qtravel.p.rapidapi.com",
             "x-rapidapi-key": "161dfc0a21msh4ed0b35c9e22b38p157c85jsn53ebc8a10b20"
         }
     }
 
-    $.ajax(settings).done(function (response) {
-        console.log(response);
+    $.ajax(settings).done(function (res) {
+        console.log(res);
     });
 
     //Wikipedia login
